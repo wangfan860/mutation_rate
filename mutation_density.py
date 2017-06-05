8b973e57fab4ca61f25c31bae8f2426c995f4dd9
wangfan860 <fan1@uchicago.edu>
2017-06-05 20:09:26 +0000

    rm handle.close()

--- mutation_density.py
+++ mutation_density.py
@@ -41,7 +41,6 @@ def create_density_csv(fai_path, blocksize, input_file, output_file):
             density = float(mc) / float(int(block[2]) + 1 - int(block[1]))
             c = '%s:%s-%s, %s, %s\n' % (block[0], block[1], block[2], mc, density)
             oh.writelines(c)
-    oh.close()
 
 def gc_element(chr, start, end, path):
     '''
@@ -53,9 +52,8 @@ def gc_element(chr, start, end, path):
         for line in handle:
             if not line.startswith("variableStep"):
                 tmp = line.rstrip('\n').split("\t")
-                if int(start) <= int(tmp[0])& int(tmp[0]) +4 <= int(end):
-                    value.append(float(tmp[1]))
-    handle.close()	
+                if int(start) <= int(tmp[0]) & int(tmp[0]) + 4 <= int(end):
+                    value.append(float(tmp[1]))	
     pos['%s:%s-%s' % (chr, start, end)] = float(sum(value))/20/float(end + 1 - start)
     return pos
 
@@ -73,7 +71,6 @@ def create_gc_csv(chr, fai_path, blocksize, input_file, output_file):
             value = gc['%s:%s-%s' % (block[0], block[1], block[2])]
             c = '%s:%s-%s, %s\n' % (block[0], block[1], block[2], value)
             oh.writelines(c)
-    oh.close()
 
 def feature_element(chr, start, end, path):
     pos = {}
@@ -112,4 +109,3 @@ def create_feature_csv(chr, fai_path, blocksize, input_file, output_file):
                 value = feature['%s:%s-%s' % (block[0], block[1], block[2])]
                 c = '%s:%s-%s, %s\n' % (block[0], block[1], block[2], value)
                 oh.writelines(c)
-    oh.close()
