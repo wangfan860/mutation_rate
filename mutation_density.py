ccb9541530705c980c35631c889e62bf67f42b81
wangfan860 <fan1@uchicago.edu>
2017-06-05 04:44:58 +0000

    add func for other gene feature

--- mutation_density.py
+++ mutation_density.py
@@ -42,6 +42,7 @@ def create_density_csv(fai_path, blocksize, input_file, output_file):
             density = float(mc) / float(int(block[2]) + 1 - int(block[1]))
             c = '%s:%s-%s, %s, %s\n' % (block[0], block[1], block[2], mc, density)
             oh.writelines(c)
+    oh.close()
 
 def gc_element(chr, start, end, path):
     '''
@@ -69,4 +70,43 @@ def create_gc_csv(chr, fai_path, blocksize, input_file, output_file):
                 value = gc['%s:%s-%s' % (block[0], block[1], block[2])]
                 c = '%s:%s-%s, %s\n' % (block[0], block[1], block[2], value)
                 oh.writelines(c)
-        oh.close()
+    oh.close()
+
+def feature_element(chr, start, end, path):
+    pos = {}
+    with open(path, 'rb') as handle:
+        value = []
+        for line in handle:
+            tmp = line.rstrip('\n').split("\t")
+            if tmp[0] == chr:
+                big_start = start
+                big_end = end
+                feature_start = int(tmp[3])
+                feature_end = int(tmp[4])
+                if big_start < feature_start and big_end < feature_start:
+                    value.append(0)
+                elif big_start <= feature_start and feature_start <= big_end < feature_end:
+                    value.append(big_end - feature_start + 1)
+                elif big_start <= feature_start and big_end >= feature_end:
+                    value.append(feature_end - feature_start + 1)
+                elif feature_start < big_start < feature_end and big_start <  big_end < feature_end:
+                    value.append(big_end - big_start + 1)
+                elif feature_start < big_start <= feature_end and big_end >=feature_end:
+                    value.append(feature_end - big_start + 1)
+                elif big_start > feature_end and big_end > feature_end:
+                    value.append(0)
+    pos['%s:%s-%s' % (chr, start, end)] = sum(value)
+    return pos
+
+def create_feature_csv(chr, fai_path, blocksize, input_file, output_file):
+    '''
+    calculate gc contents from intervals, and write to the output_file
+    '''
+    with open(output_file, 'w') as oh:
+        for i, block in enumerate(fai_chunk(fai_path, blocksize)):
+            if block[0] == chr:
+                feature = feature_element(block[0], block[1], block[2], input_file)
+                value = feature['%s:%s-%s' % (block[0], block[1], block[2])]
+                c = '%s:%s-%s, %s\n' % (block[0], block[1], block[2], value)
+                oh.writelines(c)
+    oh.close()
