7daecbae870a81d857a0fe8ca98ac9d3347630d2
wangfan860 <fan1@uchicago.edu>
2017-06-05 02:04:19 +0000

    fix create_gc func

--- mutation_density.py
+++ mutation_density.py
@@ -42,3 +42,31 @@ def create_density_csv(fai_path, blocksize, input_file, output_file):
             density = float(mc) / float(int(block[2]) + 1 - int(block[1]))
             c = '%s:%s-%s, %s, %s\n' % (block[0], block[1], block[2], mc, density)
             oh.writelines(c)
+
+def gc_element(chr, start, end, path):
+    '''
+    read gc5base from ucsc data, and calculate gc contents from intervals
+    '''
+    pos = {}
+    with open(path, 'rb') as handle:
+        value = []
+        for line in handle:
+            if not line.startswith("variableStep"):
+                tmp = line.rstrip('\n').split("\t")
+                if int(start) <= int(tmp[0])& int(tmp[0]) +4 <= int(end):
+                    value.append(float(tmp[1]))
+        pos['%s:%s-%s' % (chr, start, end)] = float(sum(value))/20/float(end + 1 - start)
+    return pos
+
+def create_gc_csv(chr, fai_path, blocksize, input_file, output_file):
+    '''
+    calculate gc contents from intervals, and write to the output_file
+    '''
+    with open(output_file, 'w') as oh:
+        for i, block in enumerate(fai_chunk(fai_path, blocksize)):
+            if block[0] == chr:
+                gc = gc_element(block[0], block[1], block[2], input_file)
+                value = gc['%s:%s-%s' % (block[0], block[1], block[2])]
+                c = '%s:%s-%s, %s\n' % (block[0], block[1], block[2], value)
+                oh.writelines(c)
+        oh.close()
